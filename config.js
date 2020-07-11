e92028fecf9ce1a3592b8c1091028fd22a9b9393
paudeldileep <dileepkpaudel@gmail.com>
2020-07-11 13:14:26 +0545

    key removed

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '302115237862939',
-        clientSecret: '90864fa6d900bfbf6f10368408f08ee4'
+        clientId: '',
+        clientSecret: ''
     }
 }
